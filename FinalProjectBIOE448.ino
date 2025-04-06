1d871e54806eeb2c968080b9754126b2ba90a193
Alice Zhou <alicezhou@Alices-MacBook-Pro-996.local>
2025-04-06 17:08:58 -0500

    Revised flagging mechanism

--- FinalProjectBIOE448.ino
+++ FinalProjectBIOE448.ino
@@ -1,9 +1,10 @@
 #include <Wire.h> // Necessary for I2C communication
-#include <LiquidCrystal.h>
-LiquidCrystal lcd(10, 8, 5, 4, 3, 2);
+#include <LiquidCrystal.h> // Necessary for LCD communication 
+LiquidCrystal lcd(10, 8, 5, 4, 3, 2); 
 float total_acceleration;
 int accel = 0x53; // I2C address for this sensor (from data sheet)
 float x, y, z;
+float xmod, ymod, zmod;
 
 // Flags and Checkpoints Initializing
 int steps = 0;
@@ -13,8 +14,8 @@ bool first_peak_detected = false;
 //unsigned long first_peak_time = 0;
 //unsigned long second_peak_time = 0;
 
-int upper_threshold = 90000;
-int lower_threshold = 70000;
+int upper_threshold = 450;
+int lower_threshold = 400;
 
 int peak_period = 0;
 
@@ -34,31 +35,32 @@ void loop() {
   Wire.write(0x32); // Prepare to get readings for sensor (address from data sheet)
   Wire.endTransmission(false);
   Wire.requestFrom(accel, 6, true); // Get readings (2 readings per direction x 3 directions = 6 values)
-  x = (Wire.read() | Wire.read() << 8); // Parse x values
-  y = (Wire.read() | Wire.read() << 8); // Parse y values
-  y = (Wire.read() | Wire.read() << 8); // Parse z values
 
-  total_acceleration = sqrt(x*x + y*y + z*z);
+  x = (int16_t)(Wire.read() | (Wire.read() << 8));
+  y = (int16_t)(Wire.read() | (Wire.read() << 8));
+  z = (int16_t)(Wire.read() | (Wire.read() << 8));
+  
+  Serial.print("x = "); // Print values
+  Serial.print(x);
+  Serial.print(", y = ");
+  Serial.print(y);
+  Serial.print(", z = ");
+  Serial.println(z);
+
+  total_acceleration = sqrt(2*x*x + 2*y*y + z*z);
   Serial.println(total_acceleration);
 
   if (total_acceleration > upper_threshold && any_peak_detected == false) {
+    steps = steps + 1;
     any_peak_detected = true;
-
-    if (first_peak_detected == false) {
-      //first_peak_time = millis();
-      first_peak_detected = true;    
-
-    } else {
-      //second_pulse_time = millis();
-      //pulse_period = second_pulse_time - first_pulse_time;        
-      steps = steps + 1;
-      first_peak_detected = false;
-    }
   }
 
-  if (total_acceleration < lower_threshold) {
+  if (total_acceleration < lower_threshold && any_peak_detected == true) {
     any_peak_detected = false;
   }
+
   Serial.println(steps);
-  delay(60);
+  delay(50);
+  lcd.setCursor(7,0);
+  lcd.print(steps);
 }
